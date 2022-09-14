f4e08fe4f1222ff48056df62e5fcf6c62952dd29
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-09-14 15:59:21 +0300

    feat(70): 5th slide task done

--- TypeScript [70-82]/70 - typescript aplinkos kūrimas/src/main.ts
+++ TypeScript [70-82]/70 - typescript aplinkos kūrimas/src/main.ts
@@ -1,10 +1,57 @@
-import { printBlue } from './helpers.js';
+type Item = {
+  title: string,
+  price: number
+};
 
-const words = ['I\'m', 'blue', 'dabudy', 'dabudaj'];
+type Person = {
+  name: string,
+  age: number,
+  married: boolean,
+};
 
-words.forEach(printBlue);
+const firstName = 'Serbentautas';
+const age = 15;
+const coins: number[] = [1, 2, 5, 10, 20, 50, 100, 200];
 
-const studentsIdetificationKeys: (number | string)[] = [];
+const item: Item = {
+  title: 'Kėglis',
+  price: 8.99,
+};
 
-studentsIdetificationKeys.push(7);
-studentsIdetificationKeys.push('9');
+const items: Item[] = [
+  item,
+  {
+    title: 'Pienas',
+    price: 0.89,
+  },
+];
+
+const person: Person = {
+  name: 'Serbentautas',
+  age: 22,
+  married: false,
+};
+
+const friends: Person[] = [
+  person,
+  {
+    name: 'Jully',
+    age: 32,
+    married: true,
+  },
+  {
+    name: 'Martin',
+    age: 36,
+    married: false,
+  },
+];
+
+console.log({
+  firstName,
+  age,
+  coins,
+  item,
+  items,
+  person,
+  friends,
+});
